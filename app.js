81aa6f5510d7e3c9ec5fcfa8f7539743e5568cbc
mekhrojY <mekhroj9595@gmail.com>
2024-10-13 00:19:38 +0900

    - reading request param - reading request query param - sending json message

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-import express from "express"
+import express, { query } from "express"
 const server = express();
-server.get("/",(req, res) => {
-    res.send("hello world")
-})
-server.listen(8080, () => {
+const port = 3000;
+server.get("/:folder",(req, res) => {
+    const folder = req.params.folder;
+    const {name, pass} = req.query;
+    console.log(`Request param: ${folder}`);
+    
+    console.log(`The requested queries: name: ${name}, pass: ${pass}`);
+    const data = {
+        reqParam: folder, 
+        queryParam: {
+            name: name,
+            pass: pass
+        }
 
-    console.log("server run!")
+
+    }
+    res.json(data);
+    // res.send(`The  requested queries: name: ${name}, pass: ${pass}`);
+
+})
+server.listen(port, () => {
+    console.log("server is running at port ", port);
 });
--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const os = require("os");
-
-// console.log(os.cpus());
-console.log("free mem: ", Number(os.freemem())/(1024*1024) + " MB");
-// console.log(os.freemem());
-// console.log(os.totalmem);
-console.log("total mem: " + Number(os.totalmem())/(1024*1024) + " MB");
-console.log(os.hostname());
-console.log("OS Type: ",os.type());
-console.log(os.version());
-console.log("Username: ", os.userInfo().username);
-console.log(os.machine());
-console.log("Platform: ",os.platform());
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello world!");
-});
-server.listen(8080, console.log("Server run!"));
+const fs = require("fs");
+console.log(fs.mkdir("./myfolder"));
