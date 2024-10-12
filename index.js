4ee20d64d15930d6006e28afccc8cf6a7b818149
mekhrojY <mekhroj9595@gmail.com>
2024-10-13 00:27:31 +0900

    - fs mkdir usage

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const fs = require("fs");
-console.log(fs.mkdir("./myfolder"));
+import fs from "fs"
+fs.mkdir('./myfolder/nested folder 1', (err) => {
+  if (err) {
+    console.error('Error creating folder:', err);
+  } else {
+    console.log('Folder created successfully');
+  }
+});
