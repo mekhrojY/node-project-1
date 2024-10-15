5df3de7cf1f731d037e170042895d8cb09eabe4f
mekhrojY <mekhroj9595@gmail.com>
2024-10-15 23:56:12 +0900

    - add todo create API -add todo get API

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
-import express, { query } from "express"
+// import express, { query } from "express"
+// const server = express();
+// const port = 8080;
+// server.get("/:folder",(req, res) => {
+//     const folder = req.params.folder;
+//     const {name, pass} = req.query;
+//     console.log(`Request param: ${folder}`);
+
+//     console.log(`The requested queries: name: ${name}, pass: ${pass}`);
+//     const data = {
+//         reqParam: folder,
+//         queryParam: {
+//             name: name,
+//             pass: pass
+//         }
+
+//     }
+//     res.json(data);
+//     // res.send(`The  requested queries: name: ${name}, pass: ${pass}`);
+
+// })
+// server.listen(port, () => {
+//     console.log("server is running at port ", port);
+// });
+
+import express from "express";
+import { v4 } from "uuid";
 const server = express();
-const port = 3000;
-server.get("/:folder",(req, res) => {
-    const folder = req.params.folder;
-    const {name, pass} = req.query;
-    console.log(`Request param: ${folder}`);
-    
-    console.log(`The requested queries: name: ${name}, pass: ${pass}`);
-    const data = {
-        reqParam: folder, 
-        queryParam: {
-            name: name,
-            pass: pass
-        }
+server.use(express.json());
 
+const todoList = [];
 
+server.get("/todo", (req, res) => {
+  todoList.forEach((item) => console.log(item));
+  res.status(200).json({ todo: todoList });
+});
+
+server.post("/todo/add", (req, res) => {
+  const { title, desc } = req.body;
+  const item = { id: v4(), title, desc };
+  todoList.push(item);
+
+  res.status(201).json({ success: true, item });
+});
+
+server.put("/todo/edit", (req, res) => {
+  // request body ichidan title, desc va idni o'qib olamiz. 
+  const { title, desc, id } = req.body;
+// todoList arrayidan find metodi orqali berilgan idi bn bir xil bo'lgan obyektni topamiz.
+// topilgan obyekti dataga beramiz.
+  const data = todoList.find((val) => {
+    console.log(val, id);
+    console.log(val.id === id);
+
+    if (val.id === id) {
+      return val;
     }
-    res.json(data);
-    // res.send(`The  requested queries: name: ${name}, pass: ${pass}`);
+  });
+  // bu javob qaytarish joyi!
+  res.status(200).json({ success: true, todo: todoList });
+});
 
-})
-server.listen(port, () => {
-    console.log("server is running at port ", port);
+server.listen(8080, () => {
+  console.log("server run port 8080");
 });
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import fs from "fs"
-fs.rmdir('./myfolder/nested folder 1', (err) => {
-  if (err) {
-    console.error('Error creating folder:', err);
-  } else {
-    console.log('Folder created successfully');
-  }
-});
+// import fs from "fs"
+// fs.mkdir('./myfolder/nested folder 1', (err) => {
+//   if (err) {
+//     console.error('Error creating folder:', err);
+//   } else {
+//     console.log('Folder created successfully');
+//   }
+// }); 
+
+console.log("\u2261"); // This will output ≡
